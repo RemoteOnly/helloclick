ce38ff04f0a3988381c6d16d0516688f3f8b9225
RemoteOnly <yixindading@126.com>
2017-08-13 11:12:37 +0900

    [ide-config] Add laravel-ide-helper package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // register laravel-ide-helper service
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 }
